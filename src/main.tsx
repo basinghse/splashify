be4e8b2f3a3a8ffc011ffc8ea01de0babf4d34c8
Jin <jin@boulevardglobal.com>
2024-05-05 16:22:42 +1000

    Inject speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles/index.css";
+import { DiffieHellmanGroup } from "crypto";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
+    {/* Collect Vercel performance metrics */}
+    <SpeedInsights />
   </React.StrictMode>
 );
